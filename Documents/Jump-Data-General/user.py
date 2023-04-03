1fecc79ced376839e68f0574a16146176e5e7adb
Jessie Baron <jessiebaron.political@gmail.com>
2023-04-03 13:09:05 -0400

    initial

---
+++ Documents/Jump-Data-General/user.py
@@ -0,0 +1,10 @@
+name = input("What's your name:")
+age = input("What's your age:")
+profession = input("Whats your profession:")
+
+print(name, age, profession)
+
+def user(name, age, profession):
+    print("Hi my name is " + name + ", I am " + age + " and I am a " + profession)
+
+print(user("Jessie", "29", "Software Engineer"))
