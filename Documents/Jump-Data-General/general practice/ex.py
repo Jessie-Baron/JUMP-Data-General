5e8ecc5e0f5036985cb5e409bf9815469f6cf697
Jessie Baron <jessiebaron.political@gmail.com>
2023-04-11 16:43:08 -0400

    class

--- Documents/Jump-Data-General/general practice/ex.py
+++
@@ -1,4 +0,0 @@
-ex = (9,1,2,3,4,5,6,7,8,9)
-
-map = map(lambda i:  i + 5, list)
-print(list(map))
---
+++ Documents/Jump-Data-General/general practice/user5.py
@@ -0,0 +1,10 @@
+
+
+class Employee():
+    def __init__(self, first_name, last_name, age, email):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+        self.email = email
+
+    
